a77e51797dd16628bf3971fcdce3e8930b52f00b
Asqulum <71020683+Asqulum@users.noreply.github.com>
2023-10-11 15:33:38 +0200

    Disable memoize, only causes headaches

--- CellularAutomata/CelAut.py
+++ CellularAutomata/CelAut.py
@@ -43,7 +43,7 @@ class CAsimulator:
                             apply_rule=self.custom_rule, memoize=False, asynch=True)
         else:
             self.cellular_automaton = evolve2d(self.field, timesteps=steps, neighbourhood=self.neighborhood,
-                                      apply_rule=self.custom_rule, memoize=True, asynch=False)
+                                      apply_rule=self.custom_rule, memoize=False, asynch=False)
 
     def output(self, save=True, file='CA.gif'):
         self.animation = plot2d_animate(self.cellular_automaton, save=save, savename=file)
